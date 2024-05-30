645e69e5869d89860b38d3b44edf50bd94a36ca0
yao <yao@hanabit.art>
2024-05-30 11:56:09 +0900

    can build ios but not work in iossimlutor

--- cmake/utils/dependencies.cmake
+++ cmake/utils/dependencies.cmake
@@ -36,10 +36,13 @@ function(add_external_project_if_missing PROJECT_NAME NAME GIT_REPO GIT_TAG VERS
         CMAKE_ARGS ""
         DOWNLOAD_COMMAND ""
 
-        CONFIGURE_COMMAND ${CMAKE_COMMAND} "${SHARED_SOURCE_DIR}/CMakeLists.txt" ${CMAKE_CONFIGURATION_ARGS}
-        BUILD_COMMAND ${CMAKE_COMMAND} --build ${ARCH_BUILD_DIR} ${CMAKE_BUILD_ARGS}
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${ARCH_BUILD_DIR}/${CMAKE_BUILD_TYPE}/ ${PREFIX_DIR}/${ARCHS}/lib/
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${ARCH_BUILD_DIR}/include ${PREFIX_DIR}/${ARCHS}/include
+        CONFIGURE_COMMAND ${CMAKE_COMMAND} "${SHARED_SOURCE_DIR}/CMakeLists.txt" ${CMAKE_CONFIGURATION_ARGS} -DCMAKE_BUILD_Type=Release
+        BUILD_COMMAND ${CMAKE_COMMAND} --build ${ARCH_BUILD_DIR} ${CMAKE_BUILD_ARGS} --config Release
+
+        # COMMAND ${CMAKE_COMMAND} -E copy_directory ${ARCH_BUILD_DIR}/${CMAKE_BUILD_TYPE}/ ${PREFIX_DIR}/${ARCHS}/lib/
+        # COMMAND ${CMAKE_COMMAND} -E copy_directory ${ARCH_BUILD_DIR}/include ${PREFIX_DIR}/${ARCHS}/include
+        INSTALL_COMMAND ${CMAKE_COMMAND} --install ${ARCH_BUILD_DIR} --prefix ${PREFIX_DIR}/${TRIPLE_NAME} --config Release
+
         DEPENDS ${NAME}_source
     )
     MESSAGE(STATUS "Add external project: ${NAME} ${OS} ${ARCHS}")
@@ -57,73 +60,74 @@ function(add_external_project_if_missing PROJECT_NAME NAME GIT_REPO GIT_TAG VERS
     # Register the include directory and library path
     # ExternalProject_Get_Property(${NAME} PREFIX_DIR)
     set(${NAME}_INCLUDE_DIRS ${PREFIX_DIR}/include)
-    set(${NAME}_LIBRARY_DIRS ${PREFIX_DIR}/${OS}-${ARCHS}/lib)
+    set(${NAME}_LIBRARY_DIRS ${PREFIX_DIR}/${TRIPLE_NAME}/lib)
 
     if(WIN32)
         if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}d.lib)
+            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}d.lib)
         else()
-            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}.lib)
+            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}.lib)
         endif()
     elseif(${APPLE})
         if(${OS} STREQUAL "IOS")
             if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-                set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}d.a)
+                set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}d.a)
             else()
-                set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}.a)
+                set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}.a)
             endif()
         else()
-            if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-                set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}d.dylib)
-            else()
-                set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}.dylib)
-            endif()
+            # TODO: Fix this
+            # if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+            #     set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}d.dylib)
+            # else()
+            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}.dylib)
+            # endif()
         endif()
     elseif(${OS} STREQUAL "Android")
         if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}d.a)
+            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}d.a)
         else()
-            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}.a)
+            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}.a)
         endif()
     elseif(EMSCRIPTEN)
         if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}d.bc)
+            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}d.bc)
         else()
-            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}.bc)
+            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}.bc)
         endif()
     elseif(UNIX)
         if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}d.a)
+            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}d.a)
         else()
-            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}.a)
+            set(${NAME}_LIBRARIES ${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}.a)
         endif()
     else()
         message(FATAL_ERROR "Unsupported OS")
     endif()
 
-    if(NOT EXISTS "${${NAME}_LIBRARIES}")
-        file(GLOB LIBRARY_FILES "${PREFIX_DIR}/${OS}-${ARCHS}/lib/lib${NAME}*")
+    # if(NOT EXISTS "${${NAME}_LIBRARIES}")
+    #     file(GLOB LIBRARY_FILES "${PREFIX_DIR}/${TRIPLE_NAME}/lib/lib${NAME}*")
 
-        # Debug message to show the list of found files
-        message(STATUS "Found library files: ${LIBRARY_FILES}")
+    #     # Debug message to show the list of found files
+    #     message(STATUS "Found library files: ${LIBRARY_FILES}")
 
-        # Check if LIBRARY_FILES is not empty before sorting and getting the first element
-        if(LIBRARY_FILES)
-            list(SORT LIBRARY_FILES)
-            list(GET LIBRARY_FILES 0 FIRST_LIBRARY_FILE)
+    #     # Check if LIBRARY_FILES is not empty before sorting and getting the first element
+    #     if(LIBRARY_FILES)
+    #         list(SORT LIBRARY_FILES)
+    #         list(GET LIBRARY_FILES 0 FIRST_LIBRARY_FILE)
 
-            if(FIRST_LIBRARY_FILE)
-                add_custom_command(TARGET ${NAME} POST_BUILD
-                    COMMAND ${CMAKE_COMMAND} -E create_symlink ${FIRST_LIBRARY_FILE} ${${NAME}_LIBRARIES}
-                    COMMENT "Creating symlink for ${${NAME}_LIBRARIES}"
-                )
-            else()
-                message(WARNING "No suitable library file found for ${NAME}")
-            endif()
-        else()
-            message(WARNING "No library files found for ${NAME}")
-        endif()
-    endif()
+    #         if(FIRST_LIBRARY_FILE)
+    #             add_custom_command(TARGET ${TRIPLE_NAME} POST_BUILD
+    #                 COMMAND ${CMAKE_COMMAND} -E create_symlink ${FIRST_LIBRARY_FILE} ${${NAME}_LIBRARIES}
+    #                 COMMENT "Creating symlink for ${${NAME}_LIBRARIES}"
+    #             )
+    #         else()
+    #             message(WARNING "No suitable library file found for ${NAME}")
+    #         endif()
+    #     else()
+    #         message(WARNING "No library files found for ${NAME}")
+    #     endif()
+    # endif()
 
     # Make the include directories and libraries available to the parent scope
     set(${NAME}_INCLUDE_DIRS ${${NAME}_INCLUDE_DIRS} PARENT_SCOPE)
